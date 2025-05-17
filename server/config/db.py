b1302763a6bb067e365f6683dadbadeb1182de80
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-05-18 00:42:51 +0500

    feat: move username password url in the env file

--- server/config/db.py
+++ server/config/db.py
@@ -1,17 +1,19 @@
 from pymongo import MongoClient
 from urllib.parse import quote_plus
+from dotenv import load_dotenv
+import os
 
-username = "shahwaizmughal"
-password = "S@n9h6AQy2rWBAr"
+load_dotenv()
 
-encoded_username = quote_plus(username)
-encoded_password = quote_plus(password)
+username = quote_plus(os.getenv("MONGO_USER"))
+password = quote_plus(os.getenv("MONGO_PASSWORD"))
+cluster = os.getenv("MONGO_CLUSTER")
+db_name = os.getenv("MONGO_DB_NAME")
 
-uri = f"mongodb+srv://{encoded_username}:{encoded_password}@cluster0.gjubc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = f"mongodb+srv://{username}:{password}@{cluster}/?retryWrites=true&w=majority&appName=Cluster0"
 
 client = MongoClient(uri)
-
-db = client['ToTo']
+db = client[db_name]
 user_collection = db['users']
 
 
