58ba85e26c746958c0819746f23ff26add29259a
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-05-18 00:22:30 +0500

    refactor: main file code

--- server/main.py
+++ server/main.py
@@ -1,10 +1,13 @@
-from flask import Flask, Blueprint
-import re
+from flask import Flask
 from routes.user import auth_routes
+from config.db import check_mongo_connection
 
 app = Flask(__name__)
-app.register_blueprint(auth_routes)
-auth_routes = Blueprint('auth_routes', __name__)
+app.register_blueprint(auth_routes) 
+
+check_mongo_connection() 
+print (" Pyhton Server is running successfully")
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  
