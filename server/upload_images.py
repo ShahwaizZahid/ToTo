fd8d2ad3b5b4b1b227d2e4d660d669b31515d351
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-05-18 14:00:06 +0500

    upload images on cloudinary and get its url

---
+++ server/upload_images.py
@@ -0,0 +1,29 @@
+import cloudinary
+import cloudinary.uploader
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load variables from .env file
+
+cloudinary.config(
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET'),
+    secure=True
+)
+
+
+def upload_image_to_cloudinary(image_path):
+    try:
+        response = cloudinary.uploader.upload(image_path)
+        # The uploaded image URL
+        url = response.get('secure_url')
+        print(url)
+        return url
+    except Exception as e:
+        print(f"Error uploading image: {e}")
+        return None
+    
+
+upload_image_to_cloudinary('assets/images/burgers/cheese_burger.jpeg')
