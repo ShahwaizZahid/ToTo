1fc04e02227ba5bec121e7330f2fbef567f95e6e
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-05-18 12:28:26 +0500

    once user login so use the user sharepreferences to again re login

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,31 +1,42 @@
+// File: main.dart
+
 import 'package:client/models/restaurant.dart';
+import 'package:client/pages/home_page.dart';
+import 'package:client/pages/login_page.dart';
 import 'package:client/pages/register_page.dart';
 import 'package:client/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Required for async in main
+
+  // Load login status from SharedPreferences
+  final prefs = await SharedPreferences.getInstance();
+  final isLoggedIn = prefs.getBool('loggedIn') ?? false;
+
   runApp(
     MultiProvider(
       providers: [
-        //   theme provider
         ChangeNotifierProvider(create: (context) => ThemeProvider()),
         ChangeNotifierProvider(create: (context) => Restaurant()),
       ],
-      child: const MyApp(), // Replace MyApp with your main app widget
+      child: MyApp(isLoggedIn: isLoggedIn),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isLoggedIn;
+  const MyApp({super.key, required this.isLoggedIn});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TOTO',
       debugShowCheckedModeBanner: false,
       theme: Provider.of<ThemeProvider>(context).themeData,
-      home: const RegisterPage(),
+      home: isLoggedIn ? const HomePage() :  RegisterPage(),
     );
   }
 }
