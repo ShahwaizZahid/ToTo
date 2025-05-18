247708575ff451c39a206ff569c6795ee5420fd0
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-05-18 11:23:30 +0500

    add: cors

--- server/main.py
+++ server/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from routes.user import auth_routes
 from config.db import check_mongo_connection
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(auth_routes) 
 
 check_mongo_connection() 
@@ -10,4 +12,4 @@ print (" Pyhton Server is running successfully")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)  
+     app.run(host="0.0.0.0", port=5001, debug=True)
