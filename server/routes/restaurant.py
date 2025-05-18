2e87dea3d0508e0aa99d74c7c859d2abc6edc54a
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-05-18 16:26:47 +0500

    feat: create addto cart api

--- server/routes/restaurant.py
+++ server/routes/restaurant.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, jsonify
-from config.db import restaurant_collection
+from flask import Blueprint, jsonify,request
+from config.db import restaurant_collection, add_cart_items
 
 menu_routes = Blueprint('menu_routes', __name__)
 
@@ -14,3 +14,27 @@ def get_menu():
         menu_items.append(item)
 
     return jsonify(menu_items), 200
+
+
+@menu_routes.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    data = request.json
+
+    try:
+        user_id = data['userId']
+        food_id = data['foodId']
+        addons = data.get('addons', [])
+
+        cart_item = {
+            'userId': user_id,
+            'foodId': food_id,
+            'addons': addons
+        }
+
+        add_cart_items.insert_one(cart_item)
+        return jsonify({'message': 'Item added to cart successfully'}), 200
+
+    except Exception as e:
+        print('Error:', e)
+        return jsonify({'error': 'Failed to add item to cart'}), 500
+
--- server/routes/user.py
+++ server/routes/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint
 from controller.user import signup , login
 
 
