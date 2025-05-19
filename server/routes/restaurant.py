77f5bb929d8378fff279c1f569410aa08a5bd00f
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-05-19 20:13:20 +0500

    add: item count increment and decrement api

--- server/routes/restaurant.py
+++ server/routes/restaurant.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-from controller.restaurant import add_to_cart, get_menu,get_cart_items
+from controller.restaurant import add_to_cart, get_menu,get_cart_items, update_cart_count
+
 
 menu_routes = Blueprint('menu_routes', __name__)
 
@@ -9,12 +10,17 @@ def get_menu_route():
     return get_menu()
    
 
-
 @menu_routes.route('/add_to_cart', methods=['POST'])
 def add_to_cart_route():
     return add_to_cart()
+
 
 @menu_routes.route('/get_cart_items', methods=['GET'])
 def get_cart_items_route():
     return get_cart_items()
    
+
+@menu_routes.route('/cart/update_count', methods=['POST'])
+def update_cart_count_route():
+    return update_cart_count()
+    
