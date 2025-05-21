8f52c596906b2b47aa9664a1d19cd89b5e7a1902
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-05-21 18:37:31 +0500

    create: about page

---
+++ client/lib/pages/about_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Theme.of(context).colorScheme.inversePrimary),
+        title: Text("About",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.inversePrimary,
+            )),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.background,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.fastfood_rounded,
+                size: 100, color: Theme.of(context).colorScheme.primary),
+            const SizedBox(height: 20),
+            Text(
+              "TOTO - Food Delivery App",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 12),
+            Text(
+              "Version 1.0.0",
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Theme.of(context).colorScheme.primary),
+            ),
+            const SizedBox(height: 30),
+            Text(
+              "TOTO is your go-to food delivery app designed for speed, simplicity, and satisfaction. Browse a variety of dishes, customize your orders, and enjoy timely deliveries with real-time tracking.",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ListTile(
+              leading: Icon(Icons.mail_outline,
+                  color: Theme.of(context).colorScheme.primary),
+              title: Text("support@totoapp.com",
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary)),
+              subtitle:  Text("Email us your feedback",style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary) ),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone_outlined,
+                  color: Theme.of(context).colorScheme.primary),
+              title: Text("+92 3086654456",
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary)),
+              subtitle:  Text("Customer Support",style: TextStyle(
+              color: Theme.of(context).colorScheme.inversePrimary)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
