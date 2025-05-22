108e00d7bf76e1bb6b824de18bf1029c6eeedbc1
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-05-22 17:01:57 +0500

    feat: create api for get app users

--- server/routes/admin_retaurant.py
+++ server/routes/admin_retaurant.py
@@ -2,11 +2,9 @@
 from bson import ObjectId
 from flask import Blueprint
 from controller.admin_restaurant import add_food
-from flask import Blueprint
 from flask import  jsonify,request
 from flask import Blueprint, jsonify, request
-from config.db import restaurant_collection
-from upload_images import upload_image_to_cloudinary
+from config.db import restaurant_collection, user_collection
 admin_restaurant_routes = Blueprint('admin_restaurant_routes', __name__)
 
 @admin_restaurant_routes.route('/api/admin/food/add', methods=['POST'])
@@ -36,3 +34,28 @@ def delete_food(food_id):
             "status": "error",
             "message": "Invalid ID or server error."
         }), 500  
+
+
+@admin_restaurant_routes.route('/api/admin/users', methods=['GET'])
+def get_all_users():
+    try:
+        users_cursor = user_collection.find()
+        users = []
+
+        for user in users_cursor:
+            users.append({
+                "_id": str(user.get("_id")),
+                "email": user.get("email"),
+                "password": user.get("password")
+            })
+
+        return jsonify({
+            "status": "success",
+            "users": users
+        }), 200
+
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": "Failed to retrieve users."
+        }), 500        
